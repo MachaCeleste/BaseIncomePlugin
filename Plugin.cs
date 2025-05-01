13f803eac533da46e6454bfda9a739b1ec72adad
Macha Celeste <shetah1@yahoo.com>
2025-05-01 11:24:08 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BaseIncomePlugin;
+
+[BepInPlugin("com.machaceleste.baseincomeplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> baseMoney;
+
+    private void Awake()
+    {
+        baseMoney = Config.Bind("Main", "Base Job Money", 145, new ConfigDescription("Sets the base money per job for the lowest rep missions, default: 145", new AcceptableValueRange<int>(100, 1000)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.baseincomeplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/MissionConfigPatch.cs
@@ -0,0 +1,16 @@
+﻿using BaseIncomePlugin;
+using HarmonyLib;
+
+[HarmonyPatch]
+public class MissionConfigPatch
+{
+    [HarmonyPatch(typeof(MissionConfig.DirectMission), "GetBaseMoneyJob")]
+    class GetBaseMoneyJobPatch
+    {
+        static bool Prefix(ref int __result)
+        {
+            __result = Plugin.baseMoney.Value;
+            return false;
+        }
+    }
+}
